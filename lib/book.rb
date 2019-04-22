e506e710b597f34226413ec10c570e8c6ee118da
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-04-22 23:46:56 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -11,5 +11,25 @@ class Book
   def author=(author)
     @author = author 
   end 
+  
+  def author 
+    @author 
+  end 
+  
+  def page_count=(num)
+    @page_count = num 
+  end 
+  
+  def page_count
+    @page_count
+  end 
+  
+  def genre=(genre)
+    @genre = genre 
+  end 
+  
+  def genre
+    @genre
+  end 
 
 end 
